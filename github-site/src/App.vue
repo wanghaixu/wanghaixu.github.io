33e51beb30638d0e83a87f35d15f9547fbaac633
wanghaixu <13610148485@163.com>
2016-09-26 19:03:06 +0800

    测试新闻类单应用

---
+++ github-site/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app" class="ub">
+    <span style="font-size:0.32rem">16px字体</span> | 
+    <span style="font-size:16px">16px字体</span>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+
+export default {
+  components: {
+    Hello
+  }
+}
+</script>
+
+<style>
+
+</style>
